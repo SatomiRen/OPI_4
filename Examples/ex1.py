342f4399c95135e4236432255e13b86868446fb6
SatomiRen <90457026+SatomiRen@users.noreply.github.com>
2022-04-28 14:45:39 +0300

    Update ex1.py

--- Examples/ex1.py
+++ Examples/ex1.py
@@ -7,10 +7,10 @@ import json
 
 if __name__ == '__main__':
     my_list = ['foo', 'bar']
-    #Вариант 1
+    # Вариант 1
     contents = json.dumps(my_list)
     with open("foo1.txt", "w", encoding="utf-8") as f:
         f.write(contents)
-    #Вариант 2
+    # Вариант 2
     with open("foo2.txt", "w", encoding="utf-8") as f:
         json.dump(my_list, f)
